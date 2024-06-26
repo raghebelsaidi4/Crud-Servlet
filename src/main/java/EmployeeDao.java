65827b4699bdd08f7f13afad1e93e0672dc62dc3
raghebelsaidi4 <raghebelsaidi416@gamil.com>
2024-06-26 13:53:56 +0300

    Crud Example using Servlet

---
+++ src/main/java/EmployeeDao.java
@@ -0,0 +1,126 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeDao {
+	public static Connection getConnection() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/crud", "root", "ragheb01276323608");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return con;
+	}
+
+	public static int save(Employee e) {
+		int status = 0;
+		try {
+			Connection con = EmployeeDao.getConnection();
+			PreparedStatement ps = con
+					.prepareStatement("insert into user905(name,password,email,country) values (?,?,?,?)");
+			ps.setString(1, e.getName());
+			ps.setString(2, e.getPassword());
+			ps.setString(3, e.getEmail());
+			ps.setString(4, e.getCountry());
+
+			status = ps.executeUpdate();
+
+			con.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return status;
+	}
+
+	public static int update(Employee e) {
+		int status = 0;
+		try {
+			Connection con = EmployeeDao.getConnection();
+			PreparedStatement ps = con
+					.prepareStatement("update user905 set name=?,password=?,email=?,country=? where id=?");
+			ps.setString(1, e.getName());
+			ps.setString(2, e.getPassword());
+			ps.setString(3, e.getEmail());
+			ps.setString(4, e.getCountry());
+			ps.setInt(5, e.getId());
+
+			status = ps.executeUpdate();
+
+			con.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return status;
+	}
+
+	public static int delete(int id) {
+		int status = 0;
+		try {
+			Connection con = EmployeeDao.getConnection();
+			PreparedStatement ps = con.prepareStatement("delete from user905 where id=?");
+			ps.setInt(1, id);
+			status = ps.executeUpdate();
+
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return status;
+	}
+
+	public static Employee getEmployeeById(int id) {
+		Employee e = new Employee();
+
+		try {
+			Connection con = EmployeeDao.getConnection();
+			PreparedStatement ps = con.prepareStatement("select * from user905 where id=?");
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				e.setId(rs.getInt(1));
+				e.setName(rs.getString(2));
+				e.setPassword(rs.getString(3));
+				e.setEmail(rs.getString(4));
+				e.setCountry(rs.getString(5));
+			}
+			con.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return e;
+	}
+
+	public static List<Employee> getAllEmployees() {
+		List<Employee> list = new ArrayList<Employee>();
+
+		try {
+			Connection con = EmployeeDao.getConnection();
+			PreparedStatement ps = con.prepareStatement("select * from user905");
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Employee e = new Employee();
+				e.setId(rs.getInt(1));
+				e.setName(rs.getString(2));
+				e.setPassword(rs.getString(3));
+				e.setEmail(rs.getString(4));
+				e.setCountry(rs.getString(5));
+				list.add(e);
+			}
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
+}
